1fcb4eb488780f9c0c183416463155f49130b8a6
Théo Laperrouse <theo.laperrouse@laposte.net>
2020-07-05 15:21:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TooGoodToGoPython
+
+## Principe et Fonctionnalités
+
+Application Python permettant de recevoir un mail lorsque un panier est disponible dans vos restaurants favoris. Utilisation de l'API de TooGoodToGo afin de récupérer les informations nécessaires
+
+## Utilisation
+
+Renseignez ses identifiants de connexion TooGoodToGo dans le fichier config.json.
+Si l'on veut recevoir un mail quand des paniers sont disponibles, remplir également l'objet mail.
+Lancer l'application
---
+++ TGTGPython.py
@@ -0,0 +1,98 @@
+import http.client
+import json
+import sys
+import requests
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import time
+
+
+baseUrl = ""
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Accept-Language": "en-US",
+    "Accept-Charset": "utf-8"
+}
+
+
+def recupTokens():
+    with open('config.json') as logs:
+        config = json.load(logs)
+        email = config['email']
+        password = config['password']
+    authURL = "https://apptoogoodtogo.com/api/auth/v1/loginByEmail"
+    jsonToPost = {
+        'device_type': 'UNKNOWN',
+        'email': email,
+        'password': password
+    }
+
+    response = requests.post(authURL, json=jsonToPost, headers=headers)
+    data = json.loads(response.text)
+    with open('config.json', 'w') as config_json:
+        config['access_token'] = data['access_token']
+        config['refresh_token'] = data['refresh_token']
+        config['user_id'] = data['startup_data']['user']['user_id']
+        json.dump(config, config_json, indent=4)
+
+    listFav(config['user_id'], config['access_token'])
+
+
+def listFav(user_id, access_token):
+    favURL = "https://apptoogoodtogo.com/api/item/v4/"
+    bearerToken = f'Bearer {access_token}'
+    headersUser = {
+        "User-Agent": "TGTG/19.12.0 (724) (Android/Unknown; Scale/3.00)",
+        "Authorization": bearerToken
+    }
+    jsonToPost = {
+        "favorites_only": True,
+        "origin": {
+            "latitude": 52.5170365,
+            "longitude": 13.3888599
+        },
+        "radius": 200,
+        "user_id": user_id
+    }
+    response = requests.post(favURL, json=jsonToPost,
+                             headers=headersUser)
+    data = json.loads(response.text)
+    paniersText = []
+    for item in data['items']:
+        nameRestaurant = item['display_name']
+        nbrePanier = item['items_available']
+        print(f'{nameRestaurant} : {nbrePanier} panier(s) disponible(s)')
+        if(nbrePanier > 0):
+            paniersText.append(
+                f'Il y a {nbrePanier} panier disponible à {nameRestaurant}')
+
+    if len(paniersText) > 0:
+        sendMail(paniersText)
+    time.sleep(600)
+    listFav(user_id, access_token)
+
+
+def sendMail(paniersText):
+    with open('config.json', 'w') as config_json:
+        gmail_user = config_json['mail']['gmail_user']
+        gmail_password = config_json['mail']['gmail_password']
+        receveir = config_json['mail']['mailReceiver']
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+
+    msg = MIMEMultipart()
+    msg['From'], msg['To'], msg['Subject'] = {
+        'From': gmail_user, 'To': receveir, 'Subject': 'Paniers Disponibles!'}
+
+    body = '\n'.join(paniersText)
+    body = MIMEText(body)
+    msg.attach(body)
+    server.login(gmail_user, gmail_password)
+    server.sendmail(gmail_user, receveir, msg.as_string())
+    server.close()
+
+
+if __name__ == '__main__':
+    recupTokens()
